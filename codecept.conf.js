e1a1c5716e31dd71b5c080104b94e6d1b7c3560d
dbmsolod <44449038+dbmsolod@users.noreply.github.com>
2023-04-02 07:44:39 +0200

    Update codecept.conf.js

--- codecept.conf.js
+++ codecept.conf.js
@@ -4,7 +4,7 @@ const {
 } = require('@codeceptjs/configure');
 // turn on headless mode when running with HEADLESS=true environment variable
 // export HEADLESS=true && npx codeceptjs run
-setHeadlessWhen(process.env.HEADLESS);
+// setHeadlessWhen(process.env.HEADLESS);
 
 // enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
 setCommonPlugins();
